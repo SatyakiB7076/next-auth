beb9af308a1926bda5f4e02fb8319e7c7507592c
SatyakiB7076 <satyakit1998@gmail.com>
2024-10-05 22:47:41 +0530

    add usermodel and dbconfig

---
+++ src/models/userModels.js
@@ -0,0 +1,40 @@
+import mongoose from 'mongoose';
+
+const userSchema =new  mongoose.Schema({
+    username: {
+        type: String,
+        required: [true,"Please provide  a username"],
+        unique: true,
+
+    },
+    email: {
+        type: String,
+        required: [true,"Please provide  a email"],
+        unique: true,
+
+    },
+    password: {
+        type: String,
+        required: [true,"Please provide  a password"],
+    },
+    isVerrified:{
+        type:Boolean,
+        default:false
+    },
+    isAdmin:{
+        type:Boolean,
+        default:false
+    },
+    forgotPasswordToken: String,
+    forgotPasswordTokenExpiry: Date,
+    varifyToken: String,
+    varifyTokenExpiry: Date, 
+
+})
+
+//if already created then use it or else create it newly
+
+const User= mongoose.models.users||mongoose.model('User',userSchema)
+
+export default User;
+
